0c31866105f5137e5f2bc1c744efecd9abf47482
Jorge Collins <jorge.collins@gmail.com>
2021-07-20 11:49:57 -0600

    Added docs

---
+++ docs/precache-manifest.7524517d57a7b8fffd22453772b44edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336b9cbc151535df57989179360e5929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9761392ac70ed253537f",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9761392ac70ed253537f",
+    "url": "/static/js/main.5b3aa16f.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
